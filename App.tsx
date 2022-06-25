5d5182ef854f0c57d085109c5c2cbbcc49622f7f
Teemu L <teemulaaks@gmail.com>
2022-06-26 00:41:20 +0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons'
+import { Link } from '@react-navigation/native';
+import { ContactList } from "./screens/ContactList";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const TopTab = createMaterialTopTabNavigator();
+
+function Feed() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+function Messages() {
+  return (
+    <TopTab.Navigator>
+      <TopTab.Screen name="Home" component={Profile} />
+      <TopTab.Screen name="Settings" component={Settings} />
+    </TopTab.Navigator>
+  );
+}
+
+function Profile() {
+  return (
+   <View></View>
+  );
+}
+
+function Profile2() {
+  return (
+   <View><Text>Test</Text></View>
+  );
+}
+
+function Settings( { navigation } ) {
+  return (
+    <View>
+      <Button title="Test button" onPress={() => navigation.navigate('TestScreen')} />
+    </View>
+  );
+}
+
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="Contacts"
+        component={ContactList}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="list" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={Messages}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="settings" size={24} color="black" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const TestScreen = () => {
+  return (
+    <View>
+         <Link to={{ screen: 'TestScreena', params: { id: 'jane' } }}>
+      Go to Jane's profile
+    </Link>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="TestScreen" component={TestScreen} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
